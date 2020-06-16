857b01cc42721900115dcaafa63b7fa261ba053f
Izzat Bahadirov <ibahadirov@linkedin.com>
2020-06-16 14:09:57 -0400

    Add missing copyright notices

--- litr-filters/src/main/java/com/linkedin/android/litr/filter/video/gl/AnimationFrameProvider.java
+++ litr-filters/src/main/java/com/linkedin/android/litr/filter/video/gl/AnimationFrameProvider.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2019 LinkedIn Corporation
+ * All Rights Reserved.
+ *
+ * Licensed under the BSD 2-Clause License (the "License").  See License in the project root for
+ * license information.
+ */
 package com.linkedin.android.litr.filter.video.gl;
 
 import android.graphics.Bitmap;
