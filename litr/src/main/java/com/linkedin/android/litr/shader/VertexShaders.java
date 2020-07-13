6bf886c9e94da93d040df93fb18654e5ba5b3f84
Izzat Bahadirov <ibahadirov@linkedin.com>
2020-07-13 19:54:19 -0400

    Move video frame rendering from GlVideoRenderer into GlFrameRenderFilter. (#40)

---
+++ litr/src/main/java/com/linkedin/android/litr/shader/VertexShaders.java
@@ -0,0 +1,15 @@
+package com.linkedin.android.litr.shader;
+
+public class VertexShaders {
+
+    public static final String DEFAULT_SHADER =
+            "uniform mat4 uMVPMatrix;\n" +
+            "uniform mat4 uSTMatrix;\n" +
+            "attribute vec4 aPosition;\n" +
+            "attribute vec4 aTextureCoord;\n" +
+            "varying vec2 vTextureCoord;\n" +
+            "void main() {\n" +
+            "  gl_Position = uMVPMatrix * aPosition;\n" +
+            "  vTextureCoord = (uSTMatrix * aTextureCoord).xy;\n" +
+            "}\n";
+}
