359df262b65750e3e042743eddb1d416e1b24e7f
Izzat Bahadirov <ibahadirov@linkedin.com>
2020-08-18 17:58:12 -0400

    Refactor base frame render filter logic into abstract class (#45)

--- litr-filters/src/main/java/com/linkedin/android/litr/filter/video/gl/GrayscaleFilter.java
+++ litr-filters/src/main/java/com/linkedin/android/litr/filter/video/gl/GrayscaleFilter.java
@@ -7,12 +7,43 @@
  */
 package com.linkedin.android.litr.filter.video.gl;
 
-import com.linkedin.android.litr.shader.FragmentShaders;
-import com.linkedin.android.litr.shader.VertexShaders;
+import android.graphics.PointF;
 
-public class GrayscaleFilter extends GlVideoFrameRenderFilter {
+import androidx.annotation.NonNull;
+
+public class GrayscaleFilter extends BaseFrameRenderFilter {
+
+    private static final String FRAGMENT_SHADER =
+            "#extension GL_OES_EGL_image_external : require\n" +
+
+            "precision mediump float;\n" +
+            "varying vec2 vTextureCoord;\n" +
+            "uniform samplerExternalOES sTexture;\n" +
+            "const highp vec3 weight = vec3(0.2125, 0.7154, 0.0721);\n" +
+
+            "void main()\n" +
+            "{\n" +
+                "float luminance = dot(texture2D(sTexture, vTextureCoord).rgb, weight);\n" +
+                "gl_FragColor = vec4(vec3(luminance), 1.0);\n" +
+            "}";
 
     public GrayscaleFilter() {
-        super(VertexShaders.DEFAULT_SHADER, FragmentShaders.GRAYSCALE_SHADER);
+        super(DEFAULT_VERTEX_SHADER, FRAGMENT_SHADER);
+    }
+
+    /**
+     * Create frame render filter with source video frame, then scale, then position and then rotate the bitmap around its center as specified.
+     * @param size size in X and Y direction, relative to target video frame
+     * @param position position of source video frame  center, in relative coordinate in 0 - 1 range
+     *                 in fourth quadrant (0,0 is top left corner)
+     * @param rotation rotation angle of overlay, relative to target video frame, counter-clockwise, in degrees
+     */
+    public GrayscaleFilter(@NonNull PointF size, @NonNull PointF position, float rotation) {
+        super(DEFAULT_VERTEX_SHADER, FRAGMENT_SHADER, size, position, rotation);
+    }
+
+    @Override
+    protected void applyCustomGlAttributes() {
+        // no need to do anything here
     }
 }
