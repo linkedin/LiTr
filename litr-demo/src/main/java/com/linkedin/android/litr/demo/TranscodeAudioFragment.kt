7f8efa7a1481a9495a16a09943492a183f30a8a4
Izzat Bahadirov <ibahadirov@linkedin.com>
2022-01-11 10:40:51 -0500

    Demo of transcoding audio file into AAC

---
+++ litr-demo/src/main/java/com/linkedin/android/litr/demo/TranscodeAudioFragment.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 LinkedIn Corporation
+ * All Rights Reserved.
+ *
+ * Licensed under the BSD 2-Clause License (the "License").  See License in the project root for
+ * license information.
+ */
+package com.linkedin.android.litr.demo
+
+import android.net.Uri
+import com.linkedin.android.litr.MediaTransformer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.linkedin.android.litr.demo.data.SourceMedia
+import com.linkedin.android.litr.demo.data.TransformationState
+import com.linkedin.android.litr.demo.data.TransformationPresenter
+import com.linkedin.android.litr.demo.data.TargetMedia
+import com.linkedin.android.litr.demo.data.TranscodingConfigPresenter
+import com.linkedin.android.litr.demo.data.TrimConfig
+import com.linkedin.android.litr.demo.databinding.FragmentTranscodeAudioBinding
+import com.linkedin.android.litr.utils.TransformationUtil
+import java.io.File
+
+class TranscodeAudioFragment : BaseTransformationFragment(), MediaPickerListener {
+
+    private lateinit var binding: FragmentTranscodeAudioBinding
+    private lateinit var mediaTransformer: MediaTransformer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaTransformer = MediaTransformer(context!!.applicationContext)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaTransformer.release()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTranscodeAudioBinding.inflate(inflater, container, false)
+
+        val sourceMedia = SourceMedia()
+        binding.sourceMedia = sourceMedia
+        binding.sectionPickAudio.buttonPickAudio.setOnClickListener { pickAudio(this@TranscodeAudioFragment) }
+        binding.transformationState = TransformationState()
+        binding.transformationPresenter = TransformationPresenter(context!!, mediaTransformer)
+
+        val targetMedia = TargetMedia()
+        val transcodingConfigPresenter = TranscodingConfigPresenter(this, targetMedia)
+        binding.transcodingConfigPresenter = transcodingConfigPresenter
+        binding.targetMedia = targetMedia
+        binding.trimConfig = TrimConfig()
+
+        return binding.root
+    }
+
+    override fun onMediaPicked(uri: Uri) {
+        binding.sourceMedia?.let { sourceMedia ->
+            updateSourceMedia(sourceMedia, uri)
+            binding.trimConfig?.let { trimConfig -> updateTrimConfig(trimConfig, sourceMedia) }
+            val targetFile = File(TransformationUtil.getTargetFileDirectory(requireContext().applicationContext),
+                "transcoded_" + TransformationUtil.getDisplayName(context!!, sourceMedia.uri))
+            binding.targetMedia?.setTargetFile(targetFile)
+            binding.targetMedia?.setTracks(sourceMedia.tracks)
+            binding.transformationState?.setState(TransformationState.STATE_IDLE)
+            binding.transformationState?.setStats(null)
+        }
+    }
+}
