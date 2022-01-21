80c76e05f81adc44d6cc02a141cfbdef89b68c3d
Izzat Bahadirov <ibahadirov@linkedin.com>
2022-01-21 13:11:50 -0500

    Oboe resampling is working

--- litr/src/main/cpp/audio-resampler.cpp
+++ litr/src/main/cpp/audio-resampler.cpp
@@ -1,5 +1,4 @@
 #include <jni.h>
-#include <android/log.h>
 #include "oboe_resampler/MultiChannelResampler.h"
 
 using namespace resampler;
@@ -37,65 +36,32 @@ Java_com_linkedin_android_litr_render_AudioRenderer_resample(
         auto inputBuffer = new float[sourceBufferSize];
         auto outputBuffer = new float[targetBufferSize];
 
-        float maxSource = -1;
         for (int index = 0; index < sourceBufferSize; index++) {
-            inputBuffer[index] = (float) sourceBuffer[index];
-            if (maxSource < inputBuffer[index]) {
-                maxSource = inputBuffer[index];
-            }
+            inputBuffer[index] = ((float) sourceBuffer[index]);
         }
-
-        int sourceBufferPos = 0;
-        int targetBufferPos = 0;
 
         int framesProcessed = 0;
         int inputFramesLeft = sourceBufferSize / channel_count;
 
         while (inputFramesLeft > 0) {
             if (audio_resampler->isWriteNeeded()) {
-//                for (int channel = 0; channel < channel_count; channel++) {
-//                    inputBuffer[channel] = (float) sourceBuffer[sourceBufferPos + channel];
-//                }
                 audio_resampler->writeNextFrame(inputBuffer);
                 inputBuffer += channel_count;
                 inputFramesLeft--;
             } else {
                 audio_resampler->readNextFrame(outputBuffer);
-//                for (int channel = 0; channel < channel_count; channel++) {
-//                    targetBuffer[targetBufferPos + channel] = (short) outputBuffer[channel];
-//                }
+
+                for (int channel = 0; channel < channel_count; channel++) {
+                    targetBuffer[framesProcessed * channel_count + channel] = (short) *(outputBuffer + channel);
+                }
+
                 outputBuffer += channel_count;
                 framesProcessed++;
             }
         }
 
-        float maxOutput = -1;
-        for (int index = 0; index < framesProcessed; index++) {
-            for (int channel = 0; channel < channel_count; channel++) {
-                if (maxOutput < outputBuffer[index * channel_count + channel]) {
-                    maxOutput = outputBuffer[index * channel_count + channel];
-                }
-                targetBuffer[index * channel_count + channel] = (short) (outputBuffer[index * channel_count + channel]);
-            }
-        }
-
-        char* log = new char[100];
-        snprintf(log, 100, "Max source %f, max output %f", maxSource, maxOutput);
-        __android_log_write(ANDROID_LOG_DEBUG, "AudioRenderer", log);
-
-//        int factor = 2;
-//        int framesProcessed = sourceBufferSize / (factor * channel_count);
-//        for (int i = 0; i < framesProcessed; i++) {
-//            for (int c = 0; c < channel_count; c++) {
-//                targetBuffer[i * channel_count + c] = sourceBuffer[i * factor * channel_count + c];
-//            }
-//        }
-
         env->ReleaseShortArrayElements(jsourceBuffer, sourceBuffer, 0);
         env->ReleaseShortArrayElements(jtargetBuffer, targetBuffer, 0);
-
-//        delete[] inputBuffer;
-//        delete[] outputBuffer;
 
         return framesProcessed;
     }
