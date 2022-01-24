7af68cf18ee0635c59ee98940771752f4d7da146
Vasiliy Kulakov <vkulakov@linkedin.com>
2022-01-24 12:48:19 -0500

    [PR] Add copyright notice to annotations

--- litr/src/main/java/com/linkedin/android/litr/Annotations.kt
+++ litr/src/main/java/com/linkedin/android/litr/Annotations.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2021 LinkedIn Corporation
+ * All Rights Reserved.
+ *
+ * Licensed under the BSD 2-Clause License (the "License").  See License in the project root for
+ * license information.
+ */
 package com.linkedin.android.litr
 
 @Retention(value = AnnotationRetention.BINARY)
