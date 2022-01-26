29d2560d52dd8cbac26d8244f5b37e838677eb10
Izzat Bahadirov <ibahadirov@linkedin.com>
2022-01-26 15:37:48 -0500

    Add resampling support in AudioRenderer

---
+++ litr/src/main/java/com/linkedin/android/litr/render/AudioResamplerFactory.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 LinkedIn Corporation
+ * All Rights Reserved.
+ *
+ * Licensed under the BSD 2-Clause License (the "License").  See License in the project root for
+ * license information.
+ */
+package com.linkedin.android.litr.render
+
+import android.media.MediaFormat
+
+internal class AudioResamplerFactory {
+
+    fun createAudioResampler(sourceMediaFormat: MediaFormat?, targetMediaFormat: MediaFormat?): AudioResampler {
+        return if (sourceMediaFormat != null &&
+            targetMediaFormat != null &&
+            sourceMediaFormat.containsKey(MediaFormat.KEY_SAMPLE_RATE) &&
+            targetMediaFormat.containsKey(MediaFormat.KEY_SAMPLE_RATE) &&
+            sourceMediaFormat.containsKey(MediaFormat.KEY_CHANNEL_COUNT) &&
+            sourceMediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE) != targetMediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE)) {
+            OboeAudioResampler(
+                sourceMediaFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT),
+                sourceMediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE),
+                targetMediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE)
+            )
+        } else {
+            PassthroughResampler()
+        }
+    }
+}
