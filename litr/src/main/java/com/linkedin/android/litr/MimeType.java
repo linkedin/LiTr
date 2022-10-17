12c45368e9bd55056e67b7c26b2c91e0bc6f3a12
Izzat Bahadirov <ibahadirov@linkedin.com>
2022-10-17 10:45:39 -0400

    Enforce transcoding of incompatible audio track(s) to compatible codecs (#220)

---
+++ litr/src/main/java/com/linkedin/android/litr/MimeType.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2022 LinkedIn Corporation
+ * All Rights Reserved.
+ *
+ * Licensed under the BSD 2-Clause License (the "License").  See License in the project root for
+ * license information.
+ */
+package com.linkedin.android.litr;
+
+public class MimeType {
+
+    public static final String AUDIO_AAC = "audio/mp4a-latm";
+    public static final String AUDIO_RAW = "audio/raw";
+    public static final String AUDIO_OPUS = "audio/opus";
+    public static final String AUDIO_VORBIS = "audio/vorbis";
+
+    public static final String VIDEO_AVC = "video/avc";
+    public static final String VIDEO_HEVC = "video/hevc";
+    public static final String VIDEO_VP8 = "video/x-vnd.on2.vp8";
+    public static final String VIDEO_VP9 = "video/x-vnd.on2.vp9";
+}
